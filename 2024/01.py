a48904def010c8f5a3789bd8ca8932d658b54778
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-12-01 14:21:59 -0600

    2do Avance

--- 2024/01.py
+++ 2024/01.py
@@ -1,13 +1,47 @@
 fp = open ("input.txt", "r")
 
-lista = fp.readlines()
+puzzle_input = fp.readlines()
 fp.close()
 
 def answer1():
     lista_a, lista_b = [], []
+    distancia = []
+    
+    for line in puzzle_input:   
+        line = line.strip().split("   ")
+        lista_a.append(int(line[0]))
+        lista_b.append(int(line[1]))
 
+    lista_a.sort()
+    lista_b.sort()
+    
+    for i in range(len(lista_a)):
+            
+        distancia.append(abs(lista_b[i]-lista_a[i]))
+        
+    distancia = sum(distancia)
+    
+    return distancia
 
+def answer2():
+    lista_a, lista_b = [], []
+    distancia = []
+    
+    for line in puzzle_input:   
+        line = line.strip().split("   ")
+        lista_a.append(int(line[0]))
+        lista_b.append(int(line[1]))
 
-    return
+    lista_a.sort()
+    lista_b.sort()
+    
+    for i in range(len(lista_a)):
+            
+        distancia.append(abs(lista_a[i]*lista_b.count(lista_a[i])))
+        
+    distancia = sum(distancia)
+    
+    return distancia
 
 print(answer1())
+print(answer2())
