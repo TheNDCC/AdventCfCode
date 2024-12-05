4afaf6346a8ce9b07fccf8ecaa929e5b271cef14
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-12-05 17:16:50 -0600

    Segunda parte dia 2

--- 2024/02.py
+++ 2024/02.py
@@ -19,24 +19,16 @@ def answer1():
     for report in puzzle_input:   
         report = report.strip().split(" ")
         
-        count += 1
-        aumenta = int(report[0]) > int(report[1])
-        for i in range(len(report)-1):
-            result = abs(int(report[i]) - int(report[i + 1]))
-            if result < 1 or result > 3:
-                count -= 1
-                break
-            if (int(report[i]) > int(report[i+1])) != aumenta:
-                count -= 1
-                break
+        if check_report(report):
+            count +=1
     return count
+
 def answer2():
     count = 0
     for report in puzzle_input:
         report = report.strip().split(" ")
         if check_report(report):
             count +=1
-            print(report)
             continue
         for n in range(len(report)):
             backup = report[n]
@@ -45,6 +37,7 @@ def answer2():
             #print(f"borrando:{report}")
             if check_report(report):
                 count += 1
+                break
             report.insert(n,backup)
     return count
 
